2086ab5fed4c39a2f4809c06ae86315187013497
Andhitia Rama <andhitia.r@gmail.com>
2018-07-31 04:48:16 +0700

    [ADD] rest_drc_api

---
+++ rest_api_drc/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'REST API',
+    'summary': '''
+        REST API (json) access to Odoo models
+    ''',
+    'description': '''
+        REST API (json) access to Odoo models
+    ''',
+    'author': 'DRC Systems India Pvt. Ltd.',
+    'website': 'http://www.drcsystems.com',
+    'category': 'Extra Tools',
+    'version': '0.1',
+    'images': ['static/description/banner.png'],
+    'depends': ['auth_oauth'],
+    'price' : 55,
+    'currency' : 'EUR',
+}
